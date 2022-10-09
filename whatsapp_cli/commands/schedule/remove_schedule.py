4ef6f182a0dbcb2043815e37b2488856093f2c0e
1TaylanOzturk <1taylanozturk@protonmail.com>
2022-10-09 12:54:29 +0300

    remove_schedule.py moved here

---
+++ whatsapp_cli/commands/schedule/remove_schedule.py
@@ -0,0 +1,17 @@
+import click
+from ...prelude.schedule.remove_schedule import remove_schedule
+
+
+@click.command()
+def schedule():
+    """Remove a schedule"""
+
+    while True:
+        schedule_name = click.prompt(
+            "Enter the name of the schedule you want to remove")
+
+        valid = remove_schedule(schedule_name)
+        if valid:
+            click.echo(f"{schedule_name} removed.")
+        else:
+            click.echo(f"{schedule_name} not found.")
