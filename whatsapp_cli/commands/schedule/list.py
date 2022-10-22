036bef9e28e6ffe6a58eeee594583ddbcc3c7eed
1TaylanOzturk <1taylanozturk@protonmail.com>
2022-10-22 17:31:37 +0300

    Implement the list command

---
+++ whatsapp_cli/commands/schedule/list.py
@@ -0,0 +1,26 @@
+import click
+import json
+from ...prelude.config import SCHEDULES_PATH
+
+
+def truncate(message: str):
+    if len(message) > 70:
+        return message[:70] + "..."
+    else:
+        return message
+
+
+@click.command()
+def schedules():
+    """List all schedules."""
+
+    with open(SCHEDULES_PATH, "r") as f:
+        schedules = json.load(f)
+
+    if not schedules:
+        click.echo("You don't have any schedule.")
+
+    for schedule in schedules:
+        name = click.style("{}".format(schedule["name"]), fg="yellow")
+        message = truncate(schedule["message"])
+        click.echo(f"⸱ {name}: {message}")
